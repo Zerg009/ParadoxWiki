55135654c1ffe85a40f7ccc68f257508a943d941
Zerg009 <semenencostanislav@gmail.com>
2024-05-22 11:04:15 +0300

    add Montyhall game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -28,14 +28,81 @@ export class GameComponent {
   ngOnInit(): void {
     this.paradoxService.paradoxChanged$().subscribe(paradox => {
       this.loadMiniGame(paradox.tech_name);
-      console.log("subscribed");
     });
+
+    var buildUrl = "assets/game/test/Build";
+    var loaderUrl = buildUrl + "/testBuild.loader.js";
+    var config = {
+      dataUrl: buildUrl + "/testBuild.data",
+      frameworkUrl: buildUrl + "/testBuild.framework.js",
+      codeUrl: buildUrl + "/testBuild.wasm",
+      streamingAssetsUrl: "StreamingAssets",
+      companyName: "DefaultCompany",
+      productName: "Test",
+      productVersion: "1.0",
+      devicePixelRatio: 0
+    };
+    let container = document.querySelector("#unity-container");
+    let canvas: HTMLElement | null = document.querySelector("#unity-canvas");
+    let loadingBar: HTMLElement | null = document.querySelector("#unity-loading-bar");
+    let progressBarFull: HTMLElement | null = document.querySelector("#unity-progress-bar-full");
+    let fullscreenButton: HTMLElement | null = document.querySelector("#unity-fullscreen-button");
+    //let mobileWarning: HTMLElement | null = document.querySelector("#unity-mobile-warning");
+    if (container && canvas && loadingBar && progressBarFull && fullscreenButton) {
+      //console.log(createUnityInstance);
+      // if (!mobileWarning) {
+      //   mobileWarning = document.createElement('div');
+      //   mobileWarning.id = "unity-mobile-warning";
+      //   mobileWarning.innerText = "The game is not optimized for mobile devices.";
+      //   container.appendChild(mobileWarning);
+      // }
+      // if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) && mobileWarning) {
+      //   container.className = "unity-mobile";
+      //   config.devicePixelRatio = 1;
+      //   mobileWarning.style.display = "block";
+      //   setTimeout(() => {
+      //     //mobileWarning?.style.display = "none";
+      //   }, 5000);
+      // } else {
+      //   canvas.style.width = "960px";
+      //   canvas.style.height = "600px";
+      // }
+      loadingBar.style.display = "block";
+      
+      createUnityInstance(canvas, config, (progress: any) => {
+        progressBarFull.style.width = 100 * progress + "%";
+        console.log("unity instance")
+      }).then((unityInstance: any) => {
+        loadingBar.style.display = "none";
+        fullscreenButton.onclick = () => {
+          unityInstance.SetFullscreen(1);
+        };
+      }).catch((message: any) => {
+        alert(message);
+      });
+    }
   }
+
+
   ngAfterViewInit() {
     console.log("Game component paradox changed: ngAfterViewInit " + this.currentParadox.tech_name);
-    this.loadMiniGame(this.currentParadox.tech_name);
+    this.loadUnityGame();
+
+    // this.loadMiniGame(this.currentParadox.tech_name);
     //window.addEventListener('resize', () => this.resizeCanvas(this.gameCanvas.nativeElement));
     //this.resizeCanvas(this.gameCanvas.nativeElement);
+  }
+  loadUnityGame(): void {
+    const unityContainer = document.getElementById('unityContainer');
+    if (unityContainer) {
+      const script = document.createElement('script');
+      script.src = 'assets/game/test/Build/testBuild.loader.js';
+      script.onload = () => {
+        // @ts-ignore
+        UnityLoader.instantiate('unityContainer', 'assets/game/test/Build/testBuild.wasm');
+      };
+      document.body.appendChild(script);
+    }
   }
   loadMiniGame(name: string) {
     console.log("Mini game: " + name);
@@ -65,13 +132,11 @@ export class GameComponent {
     componentRef.instance.gameCanvas = this.gameCanvas.nativeElement;
     this.cdr.detectChanges();
   }
-  changeState()
-  {
-    if(this.currentState === 'video')
-    {
+  changeState() {
+    if (this.currentState === 'video') {
       this.currentState = 'game';
     }
-    else{
+    else {
       this.currentState = 'video';
     }
   }
@@ -90,17 +155,17 @@ export class GameComponent {
     let canvasHeight = 360;
 
     if (width >= 3840) { // 4K resolution
-        canvas.width = 1280;
-        canvas.height = 720;
+      canvas.width = 1280;
+      canvas.height = 720;
     } else if (width >= 2560) { // 2K resolution
-        canvas.width = 960;
-        canvas.height = 540;
+      canvas.width = 960;
+      canvas.height = 540;
     } else if (width >= 1920) { // Full HD resolution
-        canvas.width = 640;
-        canvas.height = 360;
+      canvas.width = 640;
+      canvas.height = 360;
     } else if (width >= 1366) { // HD resolution
-        canvas.width = 340;
-        canvas.height = 200;
+      canvas.width = 340;
+      canvas.height = 200;
     }
   }
 }
--- src/typings/typings.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'audioControl'{
-    export function pauseAudio(): void;
-}
